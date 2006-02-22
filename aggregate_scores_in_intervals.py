ee950b1dff8a750178263a07a463f281efd556ba
James Taylor <james@jamestaylor.org>
2006-02-22 16:28:40 +0000

    "aggregate_scores_in_intervals.py" now can read the scores either from a wiggle file (using "bx.wiggle.Reader") or a directory of previously prepared FileBinnedArray format files (e.g. from "wiggle_to_chr_binned_array.py")

--- aggregate_scores_in_intervals.py
+++ aggregate_scores_in_intervals.py
@@ -1,43 +1,83 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.4
 
 """
 usage: %prog score_file interval_file [out_file] [options] 
+    -b, --binned: 'score_file' is actually a directory of binned array files
 """
 
 from __future__ import division
 
-import sys
 import psyco_full
+import sys
+import os, os.path
+from UserDict import DictMixin
 import bx.wiggle
-from bx.binned_array import BinnedArray
+from bx.binned_array import BinnedArray, FileBinnedArray
 from fpconst import isNaN
 import cookbook.doc_optparse
 import misc
 
-def read_scores( f ):
+class FileBinnedArrayDir( DictMixin ):
+    """
+    Adapter that makes a directory of FileBinnedArray files look like
+    a regular dict of BinnedArray objects. 
+    """
+    def __init__( self, dir ):
+        self.dir = dir
+        self.cache = dict()
+    def __getitem__( self, key ):
+        value = None
+        if key in self.cache:
+            value = self.cache[key]
+        else:
+            fname = os.path.join( self.dir, "%s.ba" % key )
+            if os.path.exists( fname ):
+                value = FileBinnedArray( open( fname ) )
+                self.cache[key] = value
+        if value is None:
+            raise KeyError( "File does not exist: " + fname )
+        return value
+
+def load_scores_wiggle( fname ):
+    """
+    Read a wiggle file and return a dict of BinnedArray objects keyed 
+    by chromosome.
+    """
     scores_by_chrom = dict()
-    for chrom, pos, val in bx.wiggle.Reader( f ):
+    for chrom, pos, val in bx.wiggle.Reader( misc.open_compressed( fname ) ):
         if chrom not in scores_by_chrom:
             scores_by_chrom[chrom] = BinnedArray()
         scores_by_chrom[chrom][pos] = val
     return scores_by_chrom
 
+def load_scores_ba_dir( dir ):
+    """
+    Return a dict-like object (keyed by chromosome) that returns 
+    FileBinnedArray objects created from "key.ba" files in `dir`
+    """
+    return FileBinnedArrayDir( dir )
+    
 def main():
 
     # Parse command line
     options, args = cookbook.doc_optparse.parse( __doc__ )
     try:
-        score_file = open( args[0] )
-        interval_file = open( args[1] )
+        score_fname = args[0]
+        interval_fname = args[1]
         if len( args ) > 2:
             out_file = open( args[2], 'w' )
         else:
             out_file = sys.stdout
+        binned = bool( options.binned )
     except:
         cookbook.doc_optparse.exit()
 
-    scores_by_chrom = read_scores( misc.open_compressed( sys.argv[1] ) )
-    for line in open( sys.argv[2] ):
+    if binned:
+        scores_by_chrom = load_scores_ba_dir( score_fname )
+    else:
+        scores_by_chrom = load_scores_wiggle( score_fname )
+
+    for line in open( interval_fname ):
         fields = line.split()
         chrom, start, stop = fields[0], int( fields[1] ), int( fields[2] )
         total = 0
@@ -61,8 +101,6 @@ def main():
             
         print >> out_file, chrom, start, stop, avg, min_score, max_score
 
-    score_file.close()
-    interval_file.close()
     out_file.close()
 
 if __name__ == "__main__": main()
