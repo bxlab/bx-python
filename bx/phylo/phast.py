26cfc5ca88dc1404379c95da0c8aa399548e54ac
James Taylor <james@jamestaylor.org>
2006-03-07 03:38:42 +0000

    Fixing up phast reader, removing scipy dependency (need to abstract the linalg stuff somehow), added tests. Yay tests!

--- bx/phylo/phast.py
+++ bx/phylo/phast.py
@@ -1,54 +1,41 @@
 """
 Rudimentary support for PHAST's tree model file format
-
-ALPHABET: A C G T - 
-ORDER: 0
-SUBST_MOD: HKY85+Gap
-TRAINING_LNL: -8760441383.526642
-BACKGROUND: 0.265517 0.184702 0.184801 0.265947 0.099033 
-RATE_MAT:
-...
 """
 
-from scipy import *
-from scipy.linalg import *
-
+from Numeric import *
 from cookbook import *
 
 class TreeModel:
     def __init__( self ):
         self.alphabet = None
         self.radix = 0
+        self.order = 0
+        self.subst_mod = None
         self.background = None
         self.tree = None
         self.matrix = None
-    def matrix_for_time( self, t ):
-        return expm( self.matrix * t )
-    matrix_for_time = cachedmethod( matrix_for_time )
-
-def from_file( f ):
-    input = iter( f )
-    tm = TreeModel()
-    for line in input:
-        if line.startswith( "ALPHABET:" ):
-            tm.alphabet = tuple( line.split()[1:] )    
-            tm.radix = len( tm.alphabet )
-        if line.startswith( "BACKGROUND:" ):
-            tm.background = tuple( map( float, line.split()[1:] ) )
-        if line.startswith( "TREE:" ):
-            tm.tree = line[6:].strip() 
-        if line.startswith( "RATE_MAT:" ):
-            matrix = zeros( (tm.radix,tm.radix), Float )
-            for i in range( len( tm.alphabet ) ):
-                matrix[i] = map( float, input.next().split() )
-            tm.matrix = matrix
-    return tm
-
-if __name__ == "__main__":
-    import sys
-    tm = from_file( open( sys.argv[1] ) )
-    print tm
-    print tm.alphabet
-    print tm.background
-    print tm.matrix
-    print tm.matrix_for_time( 1 )
+    ## def matrix_for_time( self, t ):
+    ##     return expm( self.matrix * t )
+    ## matrix_for_time = cachedmethod( matrix_for_time )
+    @staticmethod
+    def from_file( f ):
+        input = iter( f )
+        tm = TreeModel()
+        for line in input:
+            if line.startswith( "ALPHABET:" ):
+                tm.alphabet = tuple( line.split()[1:] )    
+                tm.radix = len( tm.alphabet )
+            if line.startswith( "ORDER:" ):
+                tm.order = int( line.split()[1] )
+            if line.startswith( "SUBST_MOD:" ):
+                tm.subst_mod = line[11:].rstrip()                   
+            if line.startswith( "BACKGROUND:" ):
+                tm.background = tuple( map( float, line.split()[1:] ) )
+            if line.startswith( "TREE:" ):
+                tm.tree = line[6:].strip() 
+            if line.startswith( "RATE_MAT:" ):
+                matrix = zeros( (tm.radix,tm.radix), Float )
+                for i in range( len( tm.alphabet ) ):
+                    matrix[i] = map( float, input.next().split() )
+                tm.matrix = matrix
+        return tm
--- tests.py
+++ tests.py
@@ -3,8 +3,10 @@ import bx.bitset_tests
 import bx.align.score_tests
 import bx.intervals.io
 import bx.phylo.newick_tests
+import bx.phylo.phast_tests
 
 suite = unittest.TestSuite( [ bx.phylo.newick_tests.suite,
+                              bx.phylo.phast_tests.suite,
                               bx.bitset_tests.suite, 
                               bx.align.score_tests.suite,
                               bx.intervals.io.suite ] )
