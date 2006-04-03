10ed616c960d5dbaa7b636d4f993cd94d096242a
Bob Harris <rsharris@bx.psu.edu>
2006-04-03 18:41:45 +0000

    (used to be test_nib.py), updated for inclusion with other bx tests

---
+++ lib/bx/seq/nib_tests.py
@@ -0,0 +1,27 @@
+import unittest
+import nib
+
+# Same sequence data as stored in test.nib
+
+test_seq = "TGGAGGCATTTGTGATTCAATAGATGCAGAAAGAAACCTTCCTAGAGCTGGCGTTCTCTAACTAAAAGTGGAAAGTTCTGAGGAATGAGGACTGTTATAAATCCCACCCCACACCGCACCTTCTCCAGGGAAGTTTCATGGCCGTGAAGAGGACAGAAAGTGAGAACCAAGATggaactgaataaacaagcttcacactgttagtttccccatatgcttaccttcccacagatgccaaccttggaggcctaagaggcctagaatattatcctttgtctgatcatttctctacaaatttattgttctttgttaagatgctacataagcccaaattctaaccacccctttgagttacccatcatcaagtttctcccatgtg"
+
+test_seq_len = len( test_seq )
+
+class NIBTestCase( unittest.TestCase ):
+
+    def test_get( self ):
+        nibfile = nib.NibFile( file( "lib/bx/seq/test.nib" ) )
+        # Try all combinations of even / odd boundaries
+        do_test_get( nibfile, 0, 10 )
+        do_test_get( nibfile, 1, 10 )
+        do_test_get( nibfile, 0, 11 )
+        do_test_get( nibfile, 1, 11 )
+        # Test near end of file also
+        do_test_get( nibfile, test_seq_len - 10, 10 )
+        do_test_get( nibfile, test_seq_len - 11, 11 )
+
+def do_test_get( nibfile, start, len ):
+    assert nibfile.get( start, len ) == test_seq[start:start+len]
+
+test_classes = [ NIBTestCase ]
+suite = unittest.TestSuite( [ unittest.makeSuite( c ) for c in test_classes ] )
