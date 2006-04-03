8b8311b53eb828233ef56bced810283b8bd3fa5f
Bob Harris <rsharris@bx.psu.edu>
2006-04-03 19:11:31 +0000

    added qdna_tests.py

--- tests.py
+++ tests.py
@@ -9,6 +9,7 @@ import bx.phylo.newick_tests
 import bx.phylo.phast_tests
 import bx.seqmapping_tests
 import bx.seq.nib_tests
+import bx.seq.qdna_tests
 
 tests = []
 
@@ -24,5 +25,6 @@ tests.append( bx.align.maf_tests.suite )
 tests.append( bx.align.score_tests.suite )
 tests.append( bx.seqmapping_tests.suite )
 tests.append( bx.seq.nib_tests.suite )
+tests.append( bx.seq.qdna_tests.suite )
 
 suite = unittest.TestSuite( tests )
