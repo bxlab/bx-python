84aba94ee4177c4e8054f6d3a02ec68e7e02b224
Bob Harris <rsharris@bx.psu.edu>
2006-04-20 19:03:58 +0000

    was out-of-date with latest alignment class; added a few prettifying options

--- scripts/maf_to_concat_fasta.py
+++ scripts/maf_to_concat_fasta.py
@@ -3,17 +3,37 @@
 """
 Read a maf and print the text as a fasta file, concatenating blocks
 
-usage %prog species1,species2 < maf_file
+usage %prog species1,species2,... [--fill="expression"] [--wrap=columns] < maf_file
 """
 
-from __future__ import division
+from optparse import OptionParser
 
 import textwrap
 import sys
 from bx.align import maf
 
 def __main__():
-    species = sys.argv[1].split(',')
+    # Parse command line arguments
+
+    parser = OptionParser()
+    parser.add_option( "--fill", action="store", default=None, type="string", help="" )
+    parser.add_option( "--wrap", action="store", default=None, type="int", help="" )
+    parser.add_option( "--nowrap", action="store_true", default=False, dest="nowrap", help="" )
+
+    ( options, args ) = parser.parse_args()
+
+    species = []
+    for arg in args: species.extend(arg.split(','))
+
+    fill = ""
+    if options.fill: fill = eval( options.fill )
+
+    wrap = 50
+    if   (options.wrap != None): wrap = options.wrap
+    elif (options.nowrap):       wrap = 0
+
+    # create the concatenated sequences
+
     texts = {}
     for s in species: texts[s] = []
     maf_reader = maf.Reader( sys.stdin )
@@ -21,15 +41,18 @@ def __main__():
         for s in species:
             c = m.get_component_by_src_start( s ) 
             if c: texts[s].append( c.text )
-            else: texts[s].append( "-" * m.text_len )
+            else: texts[s].append( "-" * m.text_size )
     for s in species:
         print ">" + s
-        print_n( "".join( texts[s] ), 50 )
+        print_n( fill.join( texts[s] ), wrap )
 
 def print_n( s, n, f = sys.stdout ):
-    p = 0
-    while p < len( s ):
-        print >> f, s[p:min(p+n,len(s))]
-        p += n
+    if (n <= 0):
+        print >> f, s
+    else:
+        p = 0
+        while p < len( s ):
+            print >> f, s[p:min(p+n,len(s))]
+            p += n
 
 if __name__ == "__main__": __main__()
