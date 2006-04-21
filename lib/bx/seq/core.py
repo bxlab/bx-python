24c40b976438d53780d86b63f1459debc30d5210
Bob Harris <rsharris@bx.psu.edu>
2006-04-21 18:26:25 +0000

    improved reporting of unrecognized sequence file formats

--- lib/bx/seq/core.py
+++ lib/bx/seq/core.py
@@ -16,7 +16,10 @@ def seq_file (file, format=None, revcomp=False, name="", gap=None):
     if   (format == "fasta"): return fasta.FastaFile (file, revcomp=revcomp, name=name, gap=gap)
     elif (format == "nib"):   return nib.NibFile     (file, revcomp=revcomp, name=name, gap=gap)
     elif (format == "qdna"):  return qdna.QdnaFile   (file, revcomp=revcomp, name=name, gap=gap)
-    else: raise "Unknown sequence format %s" % format
+    else:
+        if (format == None): format = ""
+        else:                format = " " + format
+        raise "Unknown sequence format%s in %s" % (format,file.name)
 
 
 def seq_reader (file, format=None, revcomp=False, name="", gap=None):
