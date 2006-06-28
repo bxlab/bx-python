da0ccff34b9fffd7aafcc24063e77aab3ad5ea92
Ian Schenk <ian@bx.psu.edu>
2006-06-28 19:08:08 +0000

    Updated binned_array to include a lite mode that deallocates bins, and also a BinnedArrayWriter for writing binned arrays too large for memmory.  Comments welcome.

--- lib/bx/binned_array.py
+++ lib/bx/binned_array.py
@@ -5,26 +5,31 @@ import zlib
 
 from fpconst import *
 from Numeric import *
-from RandomArray import *
+# from RandomArray import *
 from struct import *
 
 MAGIC=0x4AB04612
-VERSION=0
+# Version incremented from version 0 to version 1 by Ian Schenck, June
+# 23, 2006.  Version 1 supports different typecodes, and in doing so
+# breaks the original header format.  The new FileBinnedArray is
+# backwards compatible with version 0.
+VERSION=1
 
 MAX=512*1024*1024 
 
 class BinnedArray( object ):
-    def __init__( self, bin_size=512*1024, default=NaN, max_size=MAX ):
+    def __init__( self, bin_size=512*1024, default=NaN, max_size=MAX, typecode="f" ):
         self.max_size = max_size
         self.bin_size = bin_size
         self.nbins = int( math.ceil( ( max_size / self.bin_size ) ) )
         self.bins = [ None ] * self.nbins
         self.default = default
+        self.typecode = typecode
     def get_bin_offset( self, index ):
         return index // self.bin_size, index % self.bin_size
     def init_bin( self, index ):
         # self.bins[index] = zeros( self.bin_size ) * self.default
-        self.bins[index] = resize( array(self.default, typecode="f"), (self.bin_size,) )
+        self.bins[index] = resize( array(self.default, typecode=self.typecode), (self.bin_size,) )
     def get( self, key ):
         bin, offset = self.get_bin_offset( key )
         if self.bins[bin]:
@@ -44,7 +49,7 @@ class BinnedArray( object ):
             delta = self.bin_size - offset
             if self.bins[bin] is None:
                 if delta < size:
-                    rval.append( resize( array(self.default, typecode="f"), (delta,) ) )
+                    rval.append( resize( array(self.default, typecode=self.typecode), (delta,) ) )
                     size -= delta
                     start += delta
                 else:
@@ -72,7 +77,10 @@ class BinnedArray( object ):
         # Write header
         write_packed( f, ">5I", MAGIC, VERSION, self.max_size, self.bin_size, self.nbins )
         # Struct module can't deal with NaN and endian conversion, we'll hack around that with Numeric
-        a = array( self.default, typecode="f" ) 
+        # save type code
+        f.write( pack('c',self.typecode ) )
+        # write default value
+        a = array( self.default, typecode=self.typecode ) 
         if LittleEndian: a = a.byteswapped()
         f.write( a.tostring() )
         # Save current position (start of bin offsets)
@@ -85,7 +93,7 @@ class BinnedArray( object ):
             if bin is None: 
                 bin_pos_and_size.append( ( 0, 0 ) )
             else:
-                assert bin.typecode() == 'f'
+                assert bin.typecode() == self.typecode
                 if LittleEndian:
                     s = bin.byteswapped().tostring()
                 else:
@@ -99,18 +107,24 @@ class BinnedArray( object ):
             write_packed( f, ">2I", pos, size )
             
 class FileBinnedArray( object ):
-    def __init__( self, f):
+    def __init__( self, f, lite=False):
         self.f = f
+        self.lite = lite
         M, V, max_size, bin_size, nbins = read_packed( f, ">5I" )
         assert M == MAGIC
-        assert V == VERSION 
+        # assert less than
+        assert V <= VERSION 
         self.max_size = max_size
         self.bin_size = bin_size
         self.nbins = nbins        
         self.bins = [ None ] * self.nbins
-        # Hacking again
-        s = f.read( 4 )
-        a = fromstring( s, "f" )
+        # Read typecode
+        if V >= 1:
+            self.typecode = unpack( 'c', f.read(1) )[0]
+        else:
+            self.typecode = 'f'
+        s = f.read( calcsize( self.typecode ) )
+        a = fromstring( s, self.typecode )
         if LittleEndian: a = a.byteswapped()
         self.default = a[0]
         # Read bin sizes and offsets
@@ -126,7 +140,7 @@ class FileBinnedArray( object ):
         assert self.bin_pos[index] != 0
         self.f.seek( self.bin_pos[index] )
         raw = self.f.read( self.bin_sizes[index] )
-        a = fromstring( zlib.decompress( raw ), "f" )
+        a = fromstring( zlib.decompress( raw ), self.typecode )
         if LittleEndian:
             a = a.byteswapped()
         assert len( a ) == self.bin_size
@@ -136,6 +150,9 @@ class FileBinnedArray( object ):
         if self.bins[bin]:
             return self.bins[bin][offset]
         elif self.bin_pos[bin]:
+            # Switching bins, kick the old bins out of memmory if
+            # "lite"
+            if self.lite: self.bins = [ None ] * self.nbins
             self.load_bin( bin )
             return self.bins[bin][offset]
         else:
@@ -148,14 +165,15 @@ class FileBinnedArray( object ):
             bin, offset = self.get_bin_offset( start )
             delta = self.bin_size - offset
             if self.bins[bin] is None and self.bin_pos[bin] != 0:
+                if self.lite: self.bins = [ None ] * self.nbins
                 self.load_bin( bin )
             if self.bins[bin] is None:
                 if delta < size:
-                    rval.append( resize( array(self.default, typecode="f"), (delta,) ) )
+                    rval.append( resize( array(self.default, typecode=self.typecode), (delta,) ) )
                     size -= delta
                     start += delta
                 else:
-                    rval.append( resize( array(self.default, typecode="f"), (size,) ) )
+                    rval.append( resize( array(self.default, typecode=self.typecode), (size,) ) )
                     size = 0
             else:
                 if delta < size:
@@ -173,7 +191,73 @@ class FileBinnedArray( object ):
             return self.get_range( start, stop )
         else:
             return self.get( key )      
-    
+
+class BinnedArrayWriter( object ):
+    def __init__( self, f, bin_size=512*1024, default=NaN, max_size=MAX, typecode="f" ):
+        # All parameters in the constructor are immutable after creation
+        self.f = f
+        self.max_size = max_size
+        self.bin_size = bin_size
+        self.nbins = int( math.ceil( ( max_size / self.bin_size ) ) )
+        self.default = default
+        self.typecode = typecode
+        self.bin = 0
+        self.bin_pos = 0
+        self.bin_index = []
+        self.buffer = resize( array(self.default, typecode=self.typecode), (self.bin_size,) )
+        self.write_header()
+        # Start the first bin
+        self.bin_index = [ (self.data_offset, 0) ]
+
+    def write_header( self ):
+        self.f.seek(0)
+        # Write header
+        write_packed( self.f, ">5I", MAGIC, VERSION, self.max_size, self.bin_size, self.nbins )
+        # Struct module can't deal with NaN and endian conversion, we'll hack around that with Numeric
+        # save type code
+        self.f.write( pack('c',self.typecode ) )
+        # write default value
+        a = array( self.default, typecode=self.typecode ) 
+        if LittleEndian: a = a.byteswapped()
+        self.f.write( a.tostring() )
+        # Save current position (start of bin offsets)
+        self.index_pos = self.f.tell()
+        self.data_offset = self.index_pos + (self.nbins * calcsize( ">2I" ))
+        
+    def write_index( self ):
+        self.f.seek(self.index_pos)
+        for pos, size in self.bin_index:
+            write_packed( self.f, ">2I", pos, size )
+
+    def write( self, data ):
+        self.buffer[self.bin_pos] = data
+        self.bin_pos += 1
+        if self.bin_pos == self.bin_size:
+            self.flush()
+            self.bin_pos = 0
+            self.bin += 1
+            assert self.bin <= self.nbins
+            self.buffer = resize( array(self.default, typecode=self.typecode), (self.bin_size,) )
+            self.bin_index.append( (self.f.tell(), 0) )
+
+    def flush( self ):
+        # Flush buffer to file
+        pos, size = self.bin_index[self.bin]
+        self.f.seek( pos )
+        if LittleEndian:
+            s = self.buffer.byteswapped().tostring()
+        else:
+            s = self.buffer.tostring()
+        compressed = zlib.compress( s )
+        self.bin_index[self.bin] = ( pos, len( compressed ) )
+        self.f.write( compressed )
+
+    def finish( self ):
+        self.flush()
+        self.nbins = self.bin + 1
+        self.write_header()
+        self.write_index()
+
 def write_packed( f, pattern, *vals ):
     f.write( pack( pattern, *vals ) )
     
