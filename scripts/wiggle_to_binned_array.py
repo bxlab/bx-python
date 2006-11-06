9bb3d070de5f7f5554c95b860cce1b97c05aef4c
James Taylor <james@jamestaylor.org>
2006-11-06 22:54:09 +0000

    Allow specifying compression type on command line.

--- scripts/wiggle_to_binned_array.py
+++ scripts/wiggle_to_binned_array.py
@@ -2,6 +2,7 @@
 
 """
 usage: %prog score_file out_file
+    -c, --comp=type: compression type (none, zlib, lzo)
 """
 
 from __future__ import division
@@ -14,22 +15,22 @@ from fpconst import isNaN
 import cookbook.doc_optparse
 import misc
 
-def read_scores( f ):
-    scores_by_chrom = dict()
-
-    return scores_by_chrom
-
 def main():
     
     # Parse command line
     options, args = cookbook.doc_optparse.parse( __doc__ )
     try:
+        if options.comp:
+            comp_type = options.comp
+        else:
+            comp_type = None
         score_fname = args[0]
         out_fname = args[1]
     except:
         cookbook.doc_optparse.exit()
 
     scores = BinnedArray()
+
     ## last_chrom = None
     for i, ( chrom, pos, val ) in enumerate( bx.wiggle.Reader( misc.open_compressed( score_fname ) ) ):
         #if last_chrom is None: 
@@ -41,7 +42,10 @@ def main():
         if i % 10000 == 0: print i, "scores processed"
 
     out = open( out_fname, "w" )
-    scores.to_file( out )
+    if comp_type:
+        scores.to_file( out, comp_type=comp_type )
+    else:    
+        scores.to_file( out )
     out.close()
 
 if __name__ == "__main__": main()
