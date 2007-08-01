12315891e6fe0132e4905d11f710d4c24254b547
Bob Harris <rsharris@bx.psu.edu>
2007-08-01 15:18:52 +0000

    moved test data for bx.seq tests to common area

--- lib/bx/seq/nib_tests.py
+++ lib/bx/seq/nib_tests.py
@@ -7,18 +7,20 @@ import sys
 import os.path
 import nib
 
+test_nib = "test_data/seq_tests/test.nib"
+
 # Same sequence data as stored in test.nib
 
-test_seq = "TGGAGGCATTTGTGATTCAATAGATGCAGAAAGAAACCTTCCTAGAGCTG" \
-         + "GCGTTCTCTAACTAAAAGTGGAAAGTTCTGAGGAATGAGGACTGTTATAA" \
-         + "ATCCCACCCCACACCGCACCTTCTCCAGGGAAGTTTCATGGCCGTGAAGA" \
-         + "GGACAGAAAGTGAGAACCAAGATggaactgaataaacaagcttcacactg" \
-         + "ttagtttccccatatgcttaccttcccacagatgccaaccttggaggcct" \
-         + "aagaggcctagaatattatcctttgtctgatcatttctctacaaatttat" \
-         + "tgttctttgttaagatgctacataagcccaaattctaaccacccctttga" \
-         + "gttacccatcatcaagtttctcccatgtg"
+valid_seq = "TGGAGGCATTTGTGATTCAATAGATGCAGAAAGAAACCTTCCTAGAGCTG" \
+          + "GCGTTCTCTAACTAAAAGTGGAAAGTTCTGAGGAATGAGGACTGTTATAA" \
+          + "ATCCCACCCCACACCGCACCTTCTCCAGGGAAGTTTCATGGCCGTGAAGA" \
+          + "GGACAGAAAGTGAGAACCAAGATggaactgaataaacaagcttcacactg" \
+          + "ttagtttccccatatgcttaccttcccacagatgccaaccttggaggcct" \
+          + "aagaggcctagaatattatcctttgtctgatcatttctctacaaatttat" \
+          + "tgttctttgttaagatgctacataagcccaaattctaaccacccctttga" \
+          + "gttacccatcatcaagtttctcccatgtg"
 
-test_seq_len = len( test_seq )
+valid_seq_len = len( valid_seq )
 
 class NIBTestCase( unittest.TestCase ):
 
@@ -30,18 +32,18 @@ class NIBTestCase( unittest.TestCase ):
         sys.stdout = sys.__stdout__
 
     def test_get( self ):
-        nibfile = nib.NibFile( file( os.path.join('lib','bx','seq','test.nib'), "rb" ) )
+        nibfile = nib.NibFile( file( test_nib ) )
         # Try all combinations of even / odd boundaries
         check_get( nibfile, 0, 10 )
         check_get( nibfile, 1, 10 )
         check_get( nibfile, 0, 11 )
         check_get( nibfile, 1, 11 )
         # Test near end of file also
-        check_get( nibfile, test_seq_len - 10, 10 )
-        check_get( nibfile, test_seq_len - 11, 11 )
+        check_get( nibfile, valid_seq_len - 10, 10 )
+        check_get( nibfile, valid_seq_len - 11, 11 )
 
 def check_get( nibfile, start, len ):
-    assert nibfile.get( start, len ) == test_seq[start:start+len]
+    assert nibfile.get( start, len ) == valid_seq[start:start+len]
 
 test_classes = [ NIBTestCase ]
 suite = unittest.TestSuite( [ unittest.makeSuite( c ) for c in test_classes ] )
