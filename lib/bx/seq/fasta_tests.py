1e6d508f1a6d0afbbaa317b154b9e3064034b506
Bob Harris <rsharris@bx.psu.edu>
2007-08-01 15:18:48 +0000

    moved test data for bx.seq tests to common area

--- lib/bx/seq/fasta_tests.py
+++ lib/bx/seq/fasta_tests.py
@@ -7,18 +7,20 @@ import sys
 import os.path
 import fasta
 
+test_fa = "test_data/seq_tests/test.fa"
+
 # Same sequence data as stored in test.fa
 
-test_seq = "TGGAGGCATTTGTGATTCAATAGATGCAGAAAGAAACCTTCCTAGAGCTG" \
-         + "GCGTTCTCTAACTAAAAGTGGAAAGTTCTGAGGAATGAGGACTGTTATAA" \
-         + "ATCCCACCCCACACCGCACCTTCTCCAGGGAAGTTTCATGGCCGTGAAGA" \
-         + "GGACAGAAAGTGAGAACCAAGATggaactgaataaacaagcttcacactg" \
-         + "ttagtttccccatatgcttaccttcccacagatgccaaccttggaggcct" \
-         + "aagaggcctagaatattatcctttgtctgatcatttctctacaaatttat" \
-         + "tgttctttgttaagatgctacataagcccaaattctaaccacccctttga" \
-         + "gttacccatcatcaagtttctcccatgtg"
+valid_seq = "TGGAGGCATTTGTGATTCAATAGATGCAGAAAGAAACCTTCCTAGAGCTG" \
+          + "GCGTTCTCTAACTAAAAGTGGAAAGTTCTGAGGAATGAGGACTGTTATAA" \
+          + "ATCCCACCCCACACCGCACCTTCTCCAGGGAAGTTTCATGGCCGTGAAGA" \
+          + "GGACAGAAAGTGAGAACCAAGATggaactgaataaacaagcttcacactg" \
+          + "ttagtttccccatatgcttaccttcccacagatgccaaccttggaggcct" \
+          + "aagaggcctagaatattatcctttgtctgatcatttctctacaaatttat" \
+          + "tgttctttgttaagatgctacataagcccaaattctaaccacccctttga" \
+          + "gttacccatcatcaagtttctcccatgtg"
 
-test_seq_len = len( test_seq )
+valid_seq_len = len( valid_seq )
 
 class FASTATestCase( unittest.TestCase ):
 
@@ -30,13 +32,13 @@ class FASTATestCase( unittest.TestCase ):
     #     sys.stdout = sys.__stdout__
 
     def test_get( self ):
-        fastafile = fasta.FastaFile( file( os.path.join('lib','bx','seq','test.fa'), "rb" ) )
-        check_get(fastafile, 0, test_seq_len)
+        fastafile = fasta.FastaFile( file(test_fa, "rb" ) )
+        check_get(fastafile, 0, valid_seq_len)
         check_get(fastafile, 0, 40)
-        check_get(fastafile, test_seq_len - 40, 40)
+        check_get(fastafile, valid_seq_len - 40, 40)
 
 def check_get( fastafile, start, len ):
-    assert fastafile.get( start, len ) == test_seq[start:start+len]
+    assert fastafile.get( start, len ) == valid_seq[start:start+len]
 
 test_classes = [ FASTATestCase ]
 suite = unittest.TestSuite( [ unittest.makeSuite( c ) for c in test_classes ] )
