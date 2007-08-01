7a19bc5a1374b55f8ff7f413f9aae618f09d2c7d
Bob Harris <rsharris@bx.psu.edu>
2007-08-01 15:18:29 +0000

    moved test data for bx.seq tests to common area

--- lib/bx/seq/seq_tests.py
+++ lib/bx/seq/seq_tests.py
@@ -8,15 +8,20 @@ import os.path
 import sys
 import bx.seq, fasta_tests, nib_tests, qdna_tests
 
-test_fasta = fasta_tests.test_seq
-test_nib   = nib_tests.test_seq
-test_qdna  = qdna_tests.test_seq
+test_fa     = "test_data/seq_tests/test.fa"
+test2_fa    = "test_data/seq_tests/test2.fa"
+test_nib    = "test_data/seq_tests/test.nib"
+test_qdna   = "test_data/seq_tests/test.qdna"
+
+valid_fasta = fasta_tests.valid_seq
+valid_nib   = nib_tests.valid_seq
+valid_qdna  = qdna_tests.valid_seq
 
 # Same sequences as stored in test2.fa
 
-test2 = [("apple",      "GGCGCTGCGATAAGGTTGCGACAACACGGACCTTCTTTTGCCTACCTCTGTTCTTGGCACG"),
-         ("orange",     "CGTGCCGAGAACAGAAAATACGCCGGGCGGTGCAGTAGTATCTTGGTATCCGATATGCAGG"),
-         ("grapefruit", "CCTGCATATCGACTAGTACACCCTCCCGAGGTACCCCACCCATCCCTCTTTTCTCGGCGCG")]
+valid2_fa = [("apple",      "GGCGCTGCGATAAGGTTGCGACAACACGGACCTTCTTTTGCCTACCTCTGTTCTTGGCACG"),
+             ("orange",     "CGTGCCGAGAACAGAAAATACGCCGGGCGGTGCAGTAGTATCTTGGTATCCGATATGCAGG"),
+             ("grapefruit", "CCTGCATATCGACTAGTACACCCTCCCGAGGTACCCCACCCATCCCTCTTTTCTCGGCGCG")]
 
 class SEQTestCase (unittest.TestCase):
 
@@ -28,29 +33,29 @@ class SEQTestCase (unittest.TestCase):
         sys.stdout = sys.__stdout__
 
     def test_get_fasta (self):
-        fastafile = bx.seq.seq_file (file (os.path.join('lib','bx','seq','test.fa'),"rb"))
-        check_get (fastafile, test_fasta, 3, 40)
+        fastafile = bx.seq.seq_file (file (test_fa, "rb"))
+        check_get (fastafile, valid_fasta, 3, 40)
 
     def test_get_nib (self):
-        nibfile = bx.seq.seq_file (file (os.path.join('lib','bx','seq','test.nib'),"rb"))
-        check_get (nibfile, test_nib, 3, 40)
+        nibfile = bx.seq.seq_file (file (test_nib, "rb"))
+        check_get (nibfile, valid_nib, 3, 40)
 
     def test_get_qdna (self):
-        qdnafile = bx.seq.seq_file (file (os.path.join('lib','bx','seq','test.qdna'),"rb"))
-        check_get (qdnafile, test_qdna, 3, 40)
+        qdnafile = bx.seq.seq_file (file (test_qdna, "rb"))
+        check_get (qdnafile, valid_qdna, 3, 40)
 
     def test_get_reader (self):
-        reader = bx.seq.seq_reader (file (os.path.join('lib','bx','seq','test2.fa'),"rb"))
+        reader = bx.seq.seq_reader (file (test2_fa, "rb"))
         for (ix,seq) in enumerate(reader):
-            assert (ix < len(test2)), "FastaReader returns too many sequences"
+            assert (ix < len(valid2_fa)), "FastaReader returns too many sequences"
             text = "%s" % seq
             fields = text.split()
             assert (len(fields) == 2), "SeqReader.__str__ returns incorrect sequence string \"%s\" (%d)" % text
-            assert (fields[0] == test2[ix][0]), "FastaReader returned the wrong name (%s,%s)" % (fields[0],test2[ix][0])
-            assert (fields[1] == test2[ix][1]), "FastaReader returned the wrong text (%s,%s)" % (fields[1],test2[ix][1])
+            assert (fields[0] == valid2_fa[ix][0]), "FastaReader returned the wrong name (%s,%s)" % (fields[0],valid2_fa[ix][0])
+            assert (fields[1] == valid2_fa[ix][1]), "FastaReader returned the wrong text (%s,%s)" % (fields[1],valid2_fa[ix][1])
 
-def check_get (seqfile, test_seq, start, len):
-    assert seqfile.get (start, len) == test_seq[start:start+len]
+def check_get (seqfile, valid_seq, start, len):
+    assert seqfile.get (start, len) == valid_seq[start:start+len]
 
 test_classes = [SEQTestCase]
 suite = unittest.TestSuite ([unittest.makeSuite (c) for c in test_classes])
