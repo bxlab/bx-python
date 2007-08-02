731b5af5b03ed70998df87bf289c65ff45c06a72
Bob Harris <rsharris@bx.psu.edu>
2007-08-02 14:13:38 +0000

    further improvements, about 15% faster

--- lib/bx/seq/_nib.pyx
+++ lib/bx/seq/_nib.pyx
@@ -6,34 +6,41 @@ cdef extern from "Python.h":
 import struct, sys
 
 cdef char * NIB_I2C_TABLE 
-NIB_I2C_TABLE = "TCAGNXXXtcagnxxx"
+cdef char * NIB_I2C_TABLE_FIRST 
+cdef char * NIB_I2C_TABLE_SECOND
+#NIB_I2C_TABLE        = "TCAGNXXXtcagnxxx"
+NIB_I2C_TABLE_FIRST  = "TTTTTTTTTTTTTTTTCCCCCCCCCCCCCCCCAAAAAAAAAAAAAAAAGGGGGGGGGGGGGGGGNNNNNNNNNNNNNNNNXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXttttttttttttttttccccccccccccccccaaaaaaaaaaaaaaaaggggggggggggggggnnnnnnnnnnnnnnnnxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+NIB_I2C_TABLE_SECOND = "TCAGNXXXtcagnxxxTCAGNXXXtcagnxxxTCAGNXXXtcagnxxxTCAGNXXXtcagnxxxTCAGNXXXtcagnxxxTCAGNXXXtcagnxxxTCAGNXXXtcagnxxxTCAGNXXXtcagnxxxTCAGNXXXtcagnxxxTCAGNXXXtcagnxxxTCAGNXXXtcagnxxxTCAGNXXXtcagnxxxTCAGNXXXtcagnxxxTCAGNXXXtcagnxxxTCAGNXXXtcagnxxxTCAGNXXXtcagnxxx"
 
 def translate_raw_data( data, int start, int length ):
     """
     Data is a block read from the file that needs to be unpacked, dealing
     with end conditions based on start/length.
     """
-    cdef int i 
+    cdef int i, j
     cdef char * p_rval
-    cdef char * p_data
+    cdef unsigned char * p_data
     # Allocate string to write into
     rval = PyString_FromStringAndSize( NULL, length ) 
     # Get char pointer access to strings
     p_rval = PyString_AsString( rval )
-    p_data = PyString_AsString( data )
+    p_data = <unsigned char *> PyString_AsString( data )
     i = 0
     # Odd start
     if start & 1: 
-        p_rval[i] = NIB_I2C_TABLE[ p_data[0] & 0xF ]
+        #p_rval[i] = NIB_I2C_TABLE[ p_data[0] & 0xF ]
+        p_rval[i] = NIB_I2C_TABLE_SECOND[ p_data[0] ]
         p_data = p_data + 1
         i = 1
     # Two output values for each input value
-    while 1:
-        p_rval[i] = NIB_I2C_TABLE[ ( p_data[0] >> 4 ) & 0xF ];
-        if i == length: break
-        i = i + 1
-        p_rval[i] = NIB_I2C_TABLE[ ( p_data[0] >> 0 ) & 0xF ];
-        if i == length: break
-        i = i + 1
+    for j from 0 <= j < (length-i)/2:
+        #p_rval[i]   = NIB_I2C_TABLE[ ( p_data[0] >> 4 ) & 0xF ];
+        #p_rval[i+1] = NIB_I2C_TABLE[ ( p_data[0] >> 0 ) & 0xF ];
+        p_rval[i]   = NIB_I2C_TABLE_FIRST [ p_data[0] ]
+        p_rval[i+1] = NIB_I2C_TABLE_SECOND[ p_data[0] ]
+        i = i + 2
         p_data = p_data + 1
+    # Odd end
+    if i < length: 
+        p_rval[i] = NIB_I2C_TABLE_FIRST[ p_data[0] ]
     return rval
