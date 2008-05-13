badd07d443a94ce5f74073f4bcbff7fb130f03ff
James Taylor <james@jamestaylor.org>
2008-05-13 15:44:19 +0000

    Get script_tests working again. All tests should be passing.

--- script_tests/maf_extract_ranges_indexed_tests.py
+++ script_tests/maf_extract_ranges_indexed_tests.py
@@ -2,6 +2,6 @@ import base
 import unittest
 
 class Test( base.BaseScriptTest, unittest.TestCase ):
-    command_line = "./scripts/maf_extract_ranges_indexed.py /depot/data2/galaxy/mm8/align/multiz17way/*.maf.lzo -c -p mm8. < ${bed}" 
+    command_line = "./scripts/maf_extract_ranges_indexed.py ./test_data/maf_tests/mm8_chr7_tiny.maf -c -p mm8. < ${bed}" 
     input_bed = base.TestFile( filename="./test_data/maf_tests/dcking_ghp074.bed" )
     output_stdout = base.TestFile( filename="./test_data/maf_tests/dcking_ghp074.maf" )
