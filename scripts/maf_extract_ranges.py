53d82f1313ec170fbdaec3e8a9b303e036aacfcd
Bob Harris <rsharris@bx.psu.edu>
2008-10-28 20:10:23 +0000

    backing out latest change in lieu of testing

--- scripts/maf_extract_ranges.py
+++ scripts/maf_extract_ranges.py
@@ -51,9 +51,6 @@ def __main__():
 
     for maf in bx.align.maf.Reader( sys.stdin ):
         ref_component = maf.components[ refindex ]
-        if ref_component.strand == '-':
-            maf = block.reverse_complement()
-            ref_component = maf.components[ refindex ]
         # Find overlap with reference component
         intersections = intersecter.find( ref_component.start, ref_component.end )
         # Keep output maf ordered
