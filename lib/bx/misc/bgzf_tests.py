f165cb38e57315fcbe4049128a436c4ef743ef4b
James Taylor <james@jamestaylor.org>
2009-11-30 18:37:21 -0500

    Make BGZF test a real unittest

--- lib/bx/misc/bgzf_tests.py
+++ lib/bx/misc/bgzf_tests.py
@@ -1,9 +1,7 @@
 import bx.misc.bgzf
 
 def test_bgzf():
-    f = bx.misc.bgzf.BGZFFile( "../test_data/bgzf_tests/test.txt.gz" )
-    print f.read( 10 )
+    f = bx.misc.bgzf.BGZFFile( "test_data/bgzf_tests/test.txt.gz" )
+    assert f.read( 10 ) == "begin 644 "
     print f.seek( 0 )
-    print f.read( 10 )
-    
-test_bgzf()
+    assert f.read( 10 ) == "begin 644 "
