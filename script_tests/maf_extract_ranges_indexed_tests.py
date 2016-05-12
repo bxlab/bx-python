27eb57ce0504b7c7321671d766670badfa736b67
James Taylor <james@jamestaylor.org>
2016-05-12 12:46:12 -0400

    Fix maf_extract_ranges_indexed test by adding min columns argument -- I think this preserves the intent of the test

--- script_tests/maf_extract_ranges_indexed_tests.py
+++ script_tests/maf_extract_ranges_indexed_tests.py
@@ -2,6 +2,6 @@ import base
 import unittest
 
 class Test( base.BaseScriptTest, unittest.TestCase ):
-    command_line = "./scripts/maf_extract_ranges_indexed.py ./test_data/maf_tests/mm8_chr7_tiny.maf -c -p mm8. < ${bed}" 
+    command_line = "./scripts/maf_extract_ranges_indexed.py ./test_data/maf_tests/mm8_chr7_tiny.maf -c -m 5 -p mm8. < ${bed}" 
     input_bed = base.TestFile( filename="./test_data/maf_tests/dcking_ghp074.bed" )
     output_stdout = base.TestFile( filename="./test_data/maf_tests/dcking_ghp074.maf" )
