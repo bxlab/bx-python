46b7bf49da7120437e8f98ff2fe780b0daca320f
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2019-04-23 11:21:16 +0100

    Fix flake8 errors

--- lib/bx/align/score_tests.py
+++ lib/bx/align/score_tests.py
@@ -1,7 +1,6 @@
 """
 Tests for `bx.align.score`.
 """
-import sys
 import unittest
 
 from numpy import array, cumsum, allclose
@@ -10,22 +9,21 @@ from six import StringIO
 import bx.align.maf
 import bx.align.score
 
-aligns = [ ( "CCACTAGTTTTTAAATAATCTACTATCAAATAAAAGATTTGTTAATAATAAATTTTAAATCATTAACACTT",
-             "CCATTTGGGTTCAAAAATTGATCTATCA----------TGGTGGATTATTATTTAGCCATTAAGGACAAAT", 
-             -111 ),
-           ( "CCACTAGTTTTTAAATAATCTAC-----AATAAAAGATTTGTTAATAAT---AAATTTTAAATCATTAA-----CACTT",
-             "CCATTTGGGTTCAAAAATTGATCTATCA----------TGGTGGAT---TATTATTT-----AGCCATTAAGGACAAAT", 
-             -3626 ),
-           ( "CCACTAGTTTTTGATTC",
-             "CCATTTGGGTTC-----", 
-             -299 ),
-           ( "CTTAGTTTTTGATCACC",
-             "-----CTTGGGTTTACC", 
-             -299 ),
-           ( "gggaattgaacaatgagaacacatggacacaggaaggggaacatcacacacc----------ggggcctgttgtggggtggggggaag",
-             "ggaactagaacaagggagacacatacaaacaacaacaacaacaacacagcccttcccttcaaagagcttatagtctgatggaggagag",
-             1690 )
-         ]
+aligns = [("CCACTAGTTTTTAAATAATCTACTATCAAATAAAAGATTTGTTAATAATAAATTTTAAATCATTAACACTT",
+           "CCATTTGGGTTCAAAAATTGATCTATCA----------TGGTGGATTATTATTTAGCCATTAAGGACAAAT",
+           -111),
+          ("CCACTAGTTTTTAAATAATCTAC-----AATAAAAGATTTGTTAATAAT---AAATTTTAAATCATTAA-----CACTT",
+           "CCATTTGGGTTCAAAAATTGATCTATCA----------TGGTGGAT---TATTATTT-----AGCCATTAAGGACAAAT",
+           -3626),
+          ("CCACTAGTTTTTGATTC",
+           "CCATTTGGGTTC-----",
+           -299),
+          ("CTTAGTTTTTGATCACC",
+           "-----CTTGGGTTTACC",
+           -299),
+          ("gggaattgaacaatgagaacacatggacacaggaaggggaacatcacacacc----------ggggcctgttgtggggtggggggaag",
+           "ggaactagaacaagggagacacatacaaacaacaacaacaacaacacagcccttcccttcaaagagcttatagtctgatggaggagag",
+           1690)]
 
 mafs = """##maf
 a score=2883.0
@@ -38,54 +36,51 @@ s panTro1.chr1          49673 40 + 229575298 TGCGTGATTAATGCCTGAGATTGTGTGAAGTAAAA
 s rheMac1.SCAFFOLD45837 26063 33 -     31516 TGTGTGATTAATGCCTGAGATTGTGTGAAGTAA-------
 """
 
-nonsymm_scheme = bx.align.score.build_scoring_scheme ( """  A    C    G    T
+nonsymm_scheme = bx.align.score.build_scoring_scheme("""  A    C    G    T
                                                            91    0  -31 -123
                                                          -114  100 -125  -31
                                                           -31 -125  100 -114
-                                                         -123  -31 -114   91 """, 400, 30 )
+                                                         -123  -31 -114   91 """, 400, 30)
 
-aligns_for_nonsymm_scheme = [ ( "AAAACCCCGGGGTTTT",
-                                "ACGTACGTACGTACGT", 
-                                -580 )
-                            ]
+aligns_for_nonsymm_scheme = [("AAAACCCCGGGGTTTT",
+                              "ACGTACGTACGTACGT",
+                              -580)]
 
-
-asymm_scheme = bx.align.score.build_scoring_scheme ( """    01   02    A    C    G    T
+asymm_scheme = bx.align.score.build_scoring_scheme("""    01   02    A    C    G    T
                                                        01  200 -200  -50  100  -50  100
                                                        02 -200  200  100  -50  100  -50 """,
-                                                       0, 0, gap1='\x00' )
+                                                   0, 0, gap1='\x00')
 
-aligns_for_asymm_scheme = [ ( "\x01\x01\x01\x01\x01\x01",
-                              "ACGT\x01\x02", 
-                              100 )
-                          ]
+aligns_for_asymm_scheme = [("\x01\x01\x01\x01\x01\x01",
+                            "ACGT\x01\x02",
+                            100)]
 
 
-class BasicTests( unittest.TestCase ):
+class BasicTests(unittest.TestCase):
 
-    def test_scoring_text( self ):
+    def test_scoring_text(self):
         ss = bx.align.score.hox70
         for t1, t2, score in aligns:
-            self.assertEqual( bx.align.score.score_texts( ss, t1, t2 ), score )
-            
-    def test_align( self ):
-        ss = bx.align.score.hox70
-        for block in bx.align.maf.Reader( StringIO( mafs ) ):
-            self.assertEqual( bx.align.score.score_alignment( ss, block ), float( block.score ) )
-            
-    def test_accumulate( self ):
-        ss = bx.align.score.hox70
-        self.assert_( allclose( bx.align.score.accumulate_scores( ss, "-----CTTT", "CTTAGTTTA"  ),
-                           cumsum( array( [ -430, -30, -30, -30, -30, -31, 91, 91, -123 ] ) ) ) )
-        self.assert_( allclose( bx.align.score.accumulate_scores( ss, "-----CTTT", "CTTAGTTTA", skip_ref_gaps=True ),
-                           cumsum( array( [ -581, 91, 91, -123 ] ) ) ) )
+            self.assertEqual(bx.align.score.score_texts(ss, t1, t2), score)
 
-    def test_nonsymm_scoring( self ):
+    def test_align(self):
+        ss = bx.align.score.hox70
+        for block in bx.align.maf.Reader(StringIO(mafs)):
+            self.assertEqual(bx.align.score.score_alignment(ss, block), float(block.score))
+
+    def test_accumulate(self):
+        ss = bx.align.score.hox70
+        self.assert_(allclose(bx.align.score.accumulate_scores(ss, "-----CTTT", "CTTAGTTTA"),
+                              cumsum(array([-430, -30, -30, -30, -30, -31, 91, 91, -123]))))
+        self.assert_(allclose(bx.align.score.accumulate_scores(ss, "-----CTTT", "CTTAGTTTA", skip_ref_gaps=True),
+                              cumsum(array([-581, 91, 91, -123]))))
+
+    def test_nonsymm_scoring(self):
         ss = nonsymm_scheme
         for t1, t2, score in aligns_for_nonsymm_scheme:
-            self.assertEqual( bx.align.score.score_texts( ss, t1, t2 ), score )
+            self.assertEqual(bx.align.score.score_texts(ss, t1, t2), score)
 
-    def test_asymm_scoring( self ):
+    def test_asymm_scoring(self):
         ss = asymm_scheme
         for t1, t2, score in aligns_for_asymm_scheme:
-            self.assertEqual( bx.align.score.score_texts( ss, t1, t2 ), score )
+            self.assertEqual(bx.align.score.score_texts(ss, t1, t2), score)
--- lib/bx/bitset_tests.py
+++ lib/bx/bitset_tests.py
@@ -5,108 +5,115 @@ Tests for `bx.bitset`.
 import bx.bitset
 import unittest
 
-class AbstractTests( object ):
 
-    def assert_bits( self, bits, list ):
-        assert bits.size == len( list ), "Bitset size and verification list size do not match"
-        for i in range( bits.size ):
-            self.assertEqual( bits[i], list[i] )
+class AbstractTests(object):
 
-    def test_overflow_create( self ):
-        self.assertRaises( ValueError, self.new_bits, 4000000000 )
-        
-    def test_overflow_access( self ):
-        bits = self.new_bits( 100 )
-        self.assertRaises( IndexError, bits.set, -5 )
-        self.assertRaises( IndexError, bits.set, 110 )
+    def assert_bits(self, bits, list):
+        assert bits.size == len(list), "Bitset size and verification list size do not match"
+        for i in range(bits.size):
+            self.assertEqual(bits[i], list[i])
 
-    def test_access( self ):
+    def test_overflow_create(self):
+        self.assertRaises(ValueError, self.new_bits, 4000000000)
+
+    def test_overflow_access(self):
+        bits = self.new_bits(100)
+        self.assertRaises(IndexError, bits.set, -5)
+        self.assertRaises(IndexError, bits.set, 110)
+
+    def test_access(self):
         # Create and assert empty
-        bits = self.new_bits( 100 )
-        l = [ 0 ] * 100
-        self.assert_bits( bits, l )
+        bits = self.new_bits(100)
+        l = [0] * 100
+        self.assert_bits(bits, l)
         # Set some positions
-        for pos in ( 11, 14, 70, 16 ):
-            bits.set( pos )
-            l[ pos ] = 1
+        for pos in (11, 14, 70, 16):
+            bits.set(pos)
+            l[pos] = 1
         # Clear some positions
-        for pos in ( 14, 80, 16 ):
-            bits.clear( pos )
-            l[ pos ] = 0
-        self.assert_bits( bits, l )
+        for pos in (14, 80, 16):
+            bits.clear(pos)
+            l[pos] = 0
+        self.assert_bits(bits, l)
 
-    def test_range_access( self ):
+    def test_range_access(self):
         # Create and assert empty
-        bits = self.new_bits( 100 )
-        l = [ 0 ] * 100
-        self.assert_bits( bits, l )
+        bits = self.new_bits(100)
+        l = [0] * 100
+        self.assert_bits(bits, l)
         # Set some positions
-        for b, e in ( ( 11, 14 ), (20,75), (90,99) ):
-            bits.set_range( b, e-b)
-            for pos in range( b, e ): l[ pos ] = 1
-        self.assert_bits( bits, l )
+        for b, e in ((11, 14), (20, 75), (90, 99)):
+            bits.set_range(b, e-b)
+            for pos in range(b, e):
+                l[pos] = 1
+        self.assert_bits(bits, l)
 
-    def test_count( self ):
+    def test_count(self):
         # Create and assert empty
-        bits = self.new_bits( 100 )
+        bits = self.new_bits(100)
         # Set some positions
-        for b, e in ( ( 11, 14 ), (20,75), (90,100) ):
-            bits.set_range( b, e-b)
-        self.assertEqual( bits.count_range( 0, 0 ), 0 )
-        self.assertEqual( bits.count_range( 0, 20 ), 3 )
-        self.assertEqual( bits.count_range( 25, 25 ), 25 )
-        self.assertEqual( bits.count_range( 80, 20 ), 10 )
-        self.assertEqual( bits.count_range( 0, 100 ), 68 )
+        for b, e in ((11, 14), (20, 75), (90, 100)):
+            bits.set_range(b, e-b)
+        self.assertEqual(bits.count_range(0, 0), 0)
+        self.assertEqual(bits.count_range(0, 20), 3)
+        self.assertEqual(bits.count_range(25, 25), 25)
+        self.assertEqual(bits.count_range(80, 20), 10)
+        self.assertEqual(bits.count_range(0, 100), 68)
 
-    def test_find( self ):
+    def test_find(self):
         # Create and assert empty
-        bits = self.new_bits( 100 )
+        bits = self.new_bits(100)
         # Set some positions
-        for b, e in ( ( 11, 14 ), (20,75), (90,100) ):
-            bits.set_range( b, e-b)
+        for b, e in ((11, 14), (20, 75), (90, 100)):
+            bits.set_range(b, e-b)
         # Next set
-        self.assertEqual( bits.next_set( 0 ), 11 )
-        self.assertEqual( bits.next_set( 13 ), 13 )
-        self.assertEqual( bits.next_set( 15 ), 20 )
+        self.assertEqual(bits.next_set(0), 11)
+        self.assertEqual(bits.next_set(13), 13)
+        self.assertEqual(bits.next_set(15), 20)
         # Next clear
-        self.assertEqual( bits.next_clear( 0 ), 0 )
-        self.assertEqual( bits.next_clear( 11 ), 14 )
-        self.assertEqual( bits.next_clear( 20 ), 75 )
-        self.assertEqual( bits.next_clear( 92 ), 100 )
+        self.assertEqual(bits.next_clear(0), 0)
+        self.assertEqual(bits.next_clear(11), 14)
+        self.assertEqual(bits.next_clear(20), 75)
+        self.assertEqual(bits.next_clear(92), 100)
 
-    def test_and( self ):
-        bits1 = self.new_bits( 100 )
-        bits2 = self.new_bits( 100 )
-        bits1.set_range( 20, 40 )
-        bits2.set_range( 50, 25 )
-        bits1.iand( bits2 )
+    def test_and(self):
+        bits1 = self.new_bits(100)
+        bits2 = self.new_bits(100)
+        bits1.set_range(20, 40)
+        bits2.set_range(50, 25)
+        bits1.iand(bits2)
         l = [0]*100
-        for i in range( 50, 60 ): l[i] = 1
-        self.assert_bits( bits1, l )
-    
-    def test_or( self ):
-        bits1 = self.new_bits( 100 )
-        bits2 = self.new_bits( 100 )
-        bits1.set_range( 20, 40 )
-        bits2.set_range( 50, 25 )
-        bits1.ior( bits2 )
+        for i in range(50, 60):
+            l[i] = 1
+        self.assert_bits(bits1, l)
+
+    def test_or(self):
+        bits1 = self.new_bits(100)
+        bits2 = self.new_bits(100)
+        bits1.set_range(20, 40)
+        bits2.set_range(50, 25)
+        bits1.ior(bits2)
         l = [0]*100
-        for i in range( 20, 75 ): l[i] = 1
-        self.assert_bits( bits1, l )
-        
-    def test_not( self ):
-        bits = self.new_bits( 100 )
-        bits.set_range( 20, 40 )
+        for i in range(20, 75):
+            l[i] = 1
+        self.assert_bits(bits1, l)
+
+    def test_not(self):
+        bits = self.new_bits(100)
+        bits.set_range(20, 40)
         bits.invert()
         l = [1]*100
-        for i in range( 20, 60 ): l[i] = 0
-        self.assert_bits( bits, l )
-        
-class BitSetTests( AbstractTests, unittest.TestCase ):
-    def new_bits( self, size ):
-        return bx.bitset.BitSet( size ) 
+        for i in range(20, 60):
+            l[i] = 0
+        self.assert_bits(bits, l)
 
-class BinnedBitSetTests( AbstractTests, unittest.TestCase ):
-    def new_bits( self, size ):
-        granularity = size % 11 
-        return bx.bitset.BinnedBitSet( size, granularity )
+
+class BitSetTests(AbstractTests, unittest.TestCase):
+    def new_bits(self, size):
+        return bx.bitset.BitSet(size)
+
+
+class BinnedBitSetTests(AbstractTests, unittest.TestCase):
+    def new_bits(self, size):
+        granularity = size % 11
+        return bx.bitset.BinnedBitSet(size, granularity)
