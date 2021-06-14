3fc97b296065babf86c4b323a31e90ec5df59ffd
Lucille Delisle <lucille.delisle@epfl.ch>
2021-06-14 17:54:55 +0200

    add tests for maf_extract_ranges

--- script_tests/maf_extract_ranges_indexed_tests.py
+++ script_tests/maf_extract_ranges_indexed_tests.py
@@ -24,3 +24,9 @@ class TestAccessNotRefNotIndexed(base.BaseScriptTest, unittest.TestCase):
     command_line = "./scripts/maf_extract_ranges_indexed.py ./test_data/maf_tests/mm8_chr7_tiny_mm8_ind.maf -c -m 5 -p hg18."
     input_stdin = base.TestFile(filename="./test_data/maf_tests/hg18.bed")
     output_stdout = base.TestFile(filename="./test_data/maf_tests/empty.maf")
+
+
+class TestELines(base.BaseScriptTest, unittest.TestCase):
+    command_line = "./scripts/maf_extract_ranges_indexed.py ./test_data/maf_tests/mm10_chr12_lessspe.maf -c -m 5 -p mm10."
+    input_stdin = base.TestFile(filename="./test_data/maf_tests/mm10_chr12.bed")
+    output_stdout = base.TestFile(filename="./test_data/maf_tests/mm10_chr12_slice.maf")
