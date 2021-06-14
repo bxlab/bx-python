3b3d85d1c8a444cc12cb9ec74e6f03e7e72f10d3
Lucille Delisle <lucille.delisle@epfl.ch>
2021-06-14 16:58:33 +0200

    add tests for index and maf_extract_ranges_indexed

--- script_tests/maf_extract_ranges_indexed_tests.py
+++ script_tests/maf_extract_ranges_indexed_tests.py
@@ -7,3 +7,20 @@ class Test(base.BaseScriptTest, unittest.TestCase):
     command_line = "./scripts/maf_extract_ranges_indexed.py ./test_data/maf_tests/mm8_chr7_tiny.maf -c -m 5 -p mm8."
     input_stdin = base.TestFile(filename="./test_data/maf_tests/dcking_ghp074.bed")
     output_stdout = base.TestFile(filename="./test_data/maf_tests/dcking_ghp074.maf")
+
+class TestAccessNotRef(base.BaseScriptTest, unittest.TestCase):
+    command_line = "./scripts/maf_extract_ranges_indexed.py ./test_data/maf_tests/mm8_chr7_tiny.maf -c -m 5 -p hg18."
+    input_stdin = base.TestFile(filename="./test_data/maf_tests/hg18.bed")
+    output_stdout = base.TestFile(filename="./test_data/maf_tests/test_hg18.maf")
+
+
+class TestAccessRef(base.BaseScriptTest, unittest.TestCase):
+    command_line = "./scripts/maf_extract_ranges_indexed.py ./test_data/maf_tests/mm8_chr7_tiny_mm8_ind.maf -c -m 5 -p mm8."
+    input_stdin = base.TestFile(filename="./test_data/maf_tests/dcking_ghp074.bed")
+    output_stdout = base.TestFile(filename="./test_data/maf_tests/dcking_ghp074.maf")
+    
+
+class TestAccessNotRefNotIndexed(base.BaseScriptTest, unittest.TestCase):
+    command_line = "./scripts/maf_extract_ranges_indexed.py ./test_data/maf_tests/mm8_chr7_tiny_mm8_ind.maf -c -m 5 -p hg18."
+    input_stdin = base.TestFile(filename="./test_data/maf_tests/hg18.bed")
+    output_stdout = base.TestFile(filename="./test_data/maf_tests/empty.maf")
