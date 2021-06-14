69e06ed05c1d3bea59b4954d14eb5e5001a4bd2c
Lucille Delisle <lucille.delisle@epfl.ch>
2021-06-14 18:14:55 +0200

    add test for limit_to_species

---
+++ script_tests/maf_limit_to_species_tests.py
@@ -0,0 +1,15 @@
+import unittest
+
+import base
+
+class Test1(base.BaseScriptTest, unittest.TestCase):
+    command_line = "./scripts/maf_limit_to_species.py mm8,rn4"
+    input_stdin = base.TestFile(filename="./test_data/maf_tests/mm8_chr7_tiny_no_index.maf")
+    output_stdout = base.TestFile(filename="./test_data/maf_tests/mm8_chr7_tiny_only_mouse_rat.maf")
+
+
+class TestWithE(base.BaseScriptTest, unittest.TestCase):
+    command_line = "./scripts/maf_limit_to_species.py mm10,bosTau7,loxAfr3"
+    input_stdin = base.TestFile(filename="./test_data/maf_tests/mm10_chr12_lessspe.maf")
+    output_stdout = base.TestFile(filename="./test_data/maf_tests/mm10_chr12_lessspe_onlymouse_cow_elephant.maf")
+
