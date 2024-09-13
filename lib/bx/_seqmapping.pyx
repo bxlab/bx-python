d972ab87b9ba4859e09a8f6f399227a19d6cea86
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2024-09-13 10:46:37 +0100

    Add support for Python 3.13
    Replace use of removed C API functions `PyObject_AsReadBuffer()` and
    `PyObject_AsWriteBuffer()` with
    `PyObject_GetBuffer()`/`PyBuffer_Release()`.
    
    Fix https://github.com/bxlab/bx-python/issues/102 .

--- lib/bx/_seqmapping.pyx
+++ lib/bx/_seqmapping.pyx
@@ -2,14 +2,17 @@
 Pyrex extension classes used by `seqmapping.py`.
 """
 
-cdef extern from "stdlib.h":
-    void* malloc( size_t )
-    void free( void* )
-
-cdef extern from "Python.h":
-    int PyObject_AsReadBuffer(object, const void **, Py_ssize_t *) except -1
-    int PyObject_AsWriteBuffer(object, void **, Py_ssize_t *) except -1
-    int PyBytes_AsStringAndSize(object, char **, Py_ssize_t *) except -1
+from cpython.buffer cimport (
+    PyBUF_SIMPLE,
+    PyBUF_WRITABLE,
+    PyBuffer_Release,
+    PyObject_GetBuffer,
+)
+from cpython.bytes cimport PyBytes_AsStringAndSize
+from libc.stdlib cimport (
+    free,
+    malloc,
+)
 
 import random
 import sys
@@ -20,17 +23,17 @@ from numpy import zeros
 
 cdef class CharToIntArrayMapping:
     """Mapping for converting strings to int arrays"""
-    
+
     cdef int table[256]
     cdef int out_size
     cdef object reverse_table
-    
+
     def __cinit__( self ):
         """Init empty mapping (all characters map to -1)"""
         cdef int i
         for i from 0 <= i < 256: self.table[i] = -1
         self.out_size = 0
-        
+
     def __init__( self ):
         self.reverse_table = dict()
 
@@ -44,51 +47,59 @@ cdef class CharToIntArrayMapping:
 
     def translate( self, string ):
         """Translate 'string' and return as int array"""
-        cdef Py_ssize_t s_len, t_len
+        cdef Py_ssize_t s_len
         cdef unsigned char * s_buf
+        cdef Py_buffer t_view
         cdef int * t_buf
         # Get direct access to string
         PyBytes_AsStringAndSize( string, <char **> &s_buf, &s_len )
         # Initialize empty array
         rval = zeros( s_len, 'i' )
-        PyObject_AsWriteBuffer( rval, <void **> &t_buf, &t_len ) 
-        # Translate
-        for i from 0 <= i < s_len:
-            t_buf[i] = self.table[ s_buf[ i ] ]
-        # Done
+        if PyObject_GetBuffer(rval, &t_view, PyBUF_WRITABLE) != 0:
+            raise ValueError("Unable to get buffer")
+        try:
+            t_buf = <int *>t_view.buf
+            # Translate
+            for i from 0 <= i < s_len:
+                t_buf[i] = self.table[s_buf[i]]
+        finally:
+            PyBuffer_Release(&t_view)
         return rval
-        
+
     def translate_list( self, strings ):
         """Translate a list of strings into an int array"""
-        cdef Py_ssize_t text_len, i
-        cdef Py_ssize_t s_len, t_len
-        cdef int factor
+        cdef Py_ssize_t i, s_len, text_len
+        cdef Py_buffer t_view
+        cdef int factor = 1
         cdef unsigned char * s_buf
         cdef int * t_buf
 
         # No input, no output
         if len( strings ) < 1: return None
-        
+
         # Length of result
         text_len = len( strings[0] )
 
         # Init result array
         rval = zeros( text_len, 'i' )
-        PyObject_AsWriteBuffer( rval, <void **> &t_buf, &t_len )  
-
-        # Loop over seqs and accumulate result values
-        factor = 1
-        for string in strings:
-            PyBytes_AsStringAndSize( string, <char **> &s_buf, &s_len )
-            for i from 0 <= i < text_len:
-                if t_buf[i] >= 0: 
-                    if self.table[ s_buf[i] ] == -1: 
-                        t_buf[i] = -1
-                    else: 
-                        t_buf[i] = t_buf[i] + ( self.table[ s_buf[i] ] * factor )
-            factor = factor * self.out_size
+        if PyObject_GetBuffer(rval, &t_view, PyBUF_WRITABLE) != 0:
+            raise ValueError("Unable to get buffer")
+        try:
+            t_buf = <int *>t_view.buf
+            # Loop over seqs and accumulate result values
+            for string in strings:
+                PyBytes_AsStringAndSize(string, <char **> &s_buf, &s_len)
+                for i from 0 <= i < text_len:
+                    if t_buf[i] >= 0:
+                        if self.table[s_buf[i]] == -1:
+                            t_buf[i] = -1
+                        else:
+                            t_buf[i] += self.table[s_buf[i]] * factor
+                factor *= self.out_size
+        finally:
+            PyBuffer_Release(&t_view)
         return rval
-        
+
     def reverse_map( self, val, nseqs ):
         factor = self.out_size ** (nseqs-1)
         rval = []
@@ -98,23 +109,23 @@ cdef class CharToIntArrayMapping:
             factor = floor( factor / self.out_size )
         rval.reverse()
         return rval
-        
-    def get_out_size( self ): 
+
+    def get_out_size( self ):
         return self.out_size
-         
+
 cdef class IntToIntMapping:
-    
+
     cdef int* table
     cdef int in_size
     cdef int out_size
-    
+
     def __cinit__( self, int in_size ):
         self.in_size = in_size
         self.table = <int *> malloc( in_size * sizeof( int ) )
         if self.table == NULL: raise "Malloc Failed"
         for i from 0 <= i < in_size: self.table[i] = -1
         self.out_size = 0
-        
+
     def __dealloc__( self ):
         # sys.stderr.write( "freeing mapping_helper.IntToIntMapping\n" ); sys.stderr.flush()
         free( self.table )
@@ -127,25 +138,35 @@ cdef class IntToIntMapping:
 
     def translate( self, src ):
         """Translate `string` and return as int array"""
-        cdef Py_ssize_t s_len, t_len
+        cdef Py_buffer s_view, t_view
+        cdef Py_ssize_t s_len
         cdef int *s_buf
         cdef int *t_buf
         # Get direct access to string
-        PyObject_AsReadBuffer( src, <const void **> &s_buf, &s_len )
-        s_len = s_len // sizeof( int )
-        assert s_len == len( src ), "`src` argument must be a buffer of 32bit integers"
-        # Initialize empty array
-        rval = zeros( s_len, 'i' )
-        PyObject_AsWriteBuffer( rval, <void **> &t_buf, &t_len )
-        # Translate
-        for i from 0 <= i < s_len:
-            if s_buf[i] == -1:
-                t_buf[i] = -1
-            elif s_buf[i] >= self.in_size:
-                t_buf[i] = -1
-            else:
-                t_buf[i] = self.table[ s_buf[ i ] ]
-        # Done
+        if PyObject_GetBuffer(src, &s_view, PyBUF_SIMPLE) != 0:
+            raise ValueError("Unable to get buffer")
+        try:
+            s_len = s_view.len // sizeof(int)
+            s_buf = <int *>s_view.buf
+            assert s_len == len(src), "`src` argument must be a buffer of 32bit integers"
+            # Initialize empty array
+            rval = zeros(s_len, "i")
+            if PyObject_GetBuffer(rval, &t_view, PyBUF_WRITABLE) != 0:
+                raise ValueError("Unable to get buffer")
+            try:
+                t_buf = <int *>t_view.buf
+                # Translate
+                for i from 0 <= i < s_len:
+                    if s_buf[i] == -1:
+                        t_buf[i] = -1
+                    elif s_buf[i] >= self.in_size:
+                        t_buf[i] = -1
+                    else:
+                        t_buf[i] = self.table[s_buf[i]]
+            finally:
+                PyBuffer_Release(&t_view)
+        finally:
+            PyBuffer_Release(&s_view)
         return rval
 
     def __getitem__( self, int x ):
@@ -201,7 +222,7 @@ cdef class IntToIntMapping:
         to_split = random.randrange( count )
         count = 0
         for i from 0 <= i < self.in_size:
-            if self.table[i] == a: 
+            if self.table[i] == a:
                 if count == to_split: copy.table[i] = b
                 else: copy.table[i] = a
                 count = count + 1
@@ -223,7 +244,7 @@ cdef class IntToIntMapping:
         to_split = random.sample( range( count ), count/2 )
         count = 0
         for i from 0 <= i < self.in_size:
-            if self.table[i] == a: 
+            if self.table[i] == a:
                 if count in to_split: copy.table[i] = b
                 else: copy.table[i] = a
                 count = count + 1
@@ -231,12 +252,12 @@ cdef class IntToIntMapping:
                 copy.table[i] = self.table[i]
         return copy
 
-    def get_in_size( self ): 
+    def get_in_size( self ):
         return self.in_size
 
-    def get_out_size( self ): 
+    def get_out_size( self ):
         return self.out_size
-    
+
     def get_table( self ):
         rval = zeros( self.in_size, 'i' )
         for i in range( self.in_size ):
